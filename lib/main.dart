5891a74c9dbbfce5d408037ee7d7a90aca074c5a
Akmal Rizal <akmal.rizal22@gmail.com>
2024-03-29 03:48:11 +0800

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+import 'app.dart';
+import 'firebase_options.dart';
+
+// TODO(codelab user): Get API key
+const clientId = 'YOUR_CLIENT_ID';
+
+void main() async {
+  await dotenv.load();
+
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
